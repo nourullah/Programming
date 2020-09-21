5a8069d420ef1ebe4916a9cfc6ab029fa04ce53c
nourullah <71495666+nourullah@users.noreply.github.com>
2020-09-21 06:35:20 -0700

    started  branchesandloops

---
+++ Week 2/Day 3/BranchesAndLoops/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace BranchesAndLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random num = new Random();
+            int number;
+            number = num.Next(1, 6);
+            Console.WriteLine(number);
+            }
+
+
+
+
+
+
+
+
+
+        
+    }
+}
