d0d60cd1eb475a9bb6487710445f896651613e34
nourullah <71495666+nourullah@users.noreply.github.com>
2020-09-22 04:37:58 -0700

    complete the mission.

---
+++ Week 2/Day 3/roll to 6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace roll_to_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int number = 0;          
+            int total = 0;
+            
+            do
+            {
+                number = random.Next(1, 7);
+                Console.WriteLine($"the player rolls {number}");
+                total = number + total;
+            } 
+            while (number < 6);
+
+            Console.WriteLine($"total score {total}");
+
+        }
+
+    }
+}
